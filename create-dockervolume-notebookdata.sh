9335dca804cb919a5307605ecc97e24ddc1f34a2
ToyotakaTanaka <toyotaka.tanaka@growup-system.co.jp>
2016-12-10 11:16:32 +0900

    tf-workdata add

--- create-dockervolume-notebookdata.sh
+++ create-dockervolume-notebookdata.sh
@@ -3,5 +3,7 @@
 docker build -t notebookdata notebookdata
 docker run -d -it --name notebookdata notebookdata
 
+docker build -t tf-workdata tf-workdata
+docker run -d -it --name tf-workdata tf-workdata
 
 
--- tensorflow-docker-start.sh
+++ tensorflow-docker-start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-nvidia-docker run -d --name tensorflow -p 8888:8888 -p 6006:6006 \
+nvidia-docker run -d --restart=always --name tensorflow -p 8888:8888 -p 6006:6006 \
 --volumes-from notebookdata \
+--volumes-from tf-workdata \
 gcr.io/tensorflow/tensorflow:latest-gpu 	
 #docker exec tensorflow tensorboard --logdir=/tmp/tensorflow_log
